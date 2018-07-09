64db1eaaac951e97c0503a0968c1d82e6fdad3e7
Bruce Decker <bruce.d008@gmail.com>
2018-07-09 13:47:57 -0700

    A

--- app.js
+++ app.js
@@ -126,11 +126,9 @@ app.post('/getConfigurationbyMerchantID', function(req, res) {
 	const { BasicNotification } = require('apns2')
 	let bn = new BasicNotification("3453d878599838d3483ba40334d221dc8c9d469a2ce51852f3f46fb094f6fe21", merchantID)
  
-	try {
+	
 	  client.send(bn)
-	} catch(err) {
-	  console.error("API in getConfigurationbyMerchantID " + err.reason)
-	}
+	
 
 	//console.log("selfie is " + selfie)
 	res.send(selfie)

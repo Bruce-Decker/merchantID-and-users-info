2cc7420867d232a642e2cbf6f59ca6adea585ac2
Bruce Decker <bruce.d008@gmail.com>
2018-07-09 14:04:35 -0700

    A

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ let client = new APNS({
   defaultTopic: `com.IDXStudio.FastPassMerchant`
 })
 
+let bn = new BasicNotification("3453d878599838d3483ba40334d221dc8c9d469a2ce51852f3f46fb094f6fe21", "sdfsdfsdfsd")
+ 
+	try {
+	  client.send(bn)
+	  console.log("test for push BasicNotification")
+	} catch(err) {
+	  console.error("API in getConfigurationbyMerchantID " + err.reason)
+	}
 
 
 

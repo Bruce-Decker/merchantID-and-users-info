dd861a0cef9016e16a7185cd4da0ed8f3451053f
Bruce Decker <bruce.d008@gmail.com>
2018-07-09 13:22:25 -0700

    A

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const { APNS } = require('apns2')
 let client = new APNS({
   team: `5P3B5P74MT`,
   keyId: `123456`,
-  signingKey: fs.readFileSync(curr_dir + 'aps.p12'),
+  signingKey: fs.readFileSync(curr_dir + '/aps.p12'),
   defaultTopic: `com.IDXStudio.FastPassMerchant`
 })
 
